a8521acae0b14d3b8283e2c107ff7a7d5c3d9397
MadPapa <happyekkov@gmail.com>
2022-03-29 16:33:15 +0200

    shopping_list - forms

---
+++ Forms_Csharp/Lista_Zakupow/Lista_Zakupow/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Lista_Zakupow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.shopping_list = new System.Windows.Forms.CheckedListBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.add_product = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(64, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(283, 79);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Shopping";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(366, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 79);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "List";
+            // 
+            // shopping_list
+            // 
+            this.shopping_list.BackColor = System.Drawing.Color.Gray;
+            this.shopping_list.Font = new System.Drawing.Font("MV Boli", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.shopping_list.ForeColor = System.Drawing.Color.White;
+            this.shopping_list.FormattingEnabled = true;
+            this.shopping_list.Location = new System.Drawing.Point(6, 209);
+            this.shopping_list.Margin = new System.Windows.Forms.Padding(0);
+            this.shopping_list.Name = "shopping_list";
+            this.shopping_list.Size = new System.Drawing.Size(617, 324);
+            this.shopping_list.TabIndex = 3;
+            // 
+            // btn_add
+            // 
+            this.btn_add.BackColor = System.Drawing.Color.Lime;
+            this.btn_add.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_add.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.btn_add.FlatAppearance.BorderSize = 3;
+            this.btn_add.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LimeGreen;
+            this.btn_add.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_add.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add.ForeColor = System.Drawing.Color.Black;
+            this.btn_add.Location = new System.Drawing.Point(9, 680);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(185, 60);
+            this.btn_add.TabIndex = 4;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = false;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.BackColor = System.Drawing.Color.Lime;
+            this.btnRemove.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRemove.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.btnRemove.FlatAppearance.BorderSize = 3;
+            this.btnRemove.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LimeGreen;
+            this.btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRemove.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.ForeColor = System.Drawing.Color.Black;
+            this.btnRemove.Location = new System.Drawing.Point(222, 680);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(185, 60);
+            this.btnRemove.TabIndex = 5;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Lime;
+            this.btnClear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClear.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.btnClear.FlatAppearance.BorderSize = 3;
+            this.btnClear.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LimeGreen;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.Black;
+            this.btnClear.Location = new System.Drawing.Point(435, 680);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(185, 60);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // add_product
+            // 
+            this.add_product.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.add_product.Location = new System.Drawing.Point(12, 621);
+            this.add_product.Name = "add_product";
+            this.add_product.Size = new System.Drawing.Size(608, 53);
+            this.add_product.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(90, 559);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(458, 40);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Describe your product bellow";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(632, 753);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.add_product);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.shopping_list);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shopping List v0.0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckedListBox shopping_list;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox add_product;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Forms_Csharp/Lista_Zakupow/Lista_Zakupow/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lista_Zakupow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void updateInput()
+        {
+            add_product.Text = "";
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            if (add_product.Text == "")
+                MessageBox.Show("Pusta wartosc", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else 
+            { 
+                if(shopping_list.Items.Contains(add_product.Text))
+                {
+                    DialogResult result = MessageBox.Show("Produkt jest już na liście czy mimo to chcesz go dodać?", "Inforamtion", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                    if (result == DialogResult.Yes)
+                    {
+                        shopping_list.Items.Add(add_product.Text);
+                        updateInput();
+                    }
+                    else
+                    {
+                        updateInput();
+                        return;
+                    }
+                }
+                else
+                {
+                    shopping_list.Items.Add(add_product.Text);
+                    updateInput();
+                }
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            int item = shopping_list.SelectedIndex;
+            if (item == -1)
+            {
+                MessageBox.Show("Nie wybrano żadnego elementu", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                shopping_list.Items.RemoveAt(item);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Czy na pewno chcesz usunąć wszystkie elementy z listy zakupów", "Warning", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+                shopping_list.Items.Clear();
+            else
+                return;
+        }
+
+    }
+}
